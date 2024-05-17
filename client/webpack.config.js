f130f88f608911ed7692fa72d8cc0ebbfd6c6f51
wellheythereMJ <abetterplace@outlook.com>
2024-05-17 14:02:41 -0700

    updated wepack, package.json, added gitignore

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,51 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+            new HtmlWebpackPlugin({
+              template: './index.html',
+              title: 'Text Editor'
+            }),
+            new InjectManifest({
+              swSrc: './src-sw.js',
+              swDest: 'src-sw.js',
+            }),
+            new WebpackPwaManifest({
+              fingerprints: false,
+              inject: true,
+              name: 'Text Editor',
+              short_name: 'Edit',
+              description: 'Edit your text!',
+              background_color: '#272822',
+              theme_color: '#31a9e1',
+              start_url: './',
+              publicPath: './',
+              icons: [
+                {
+                  src: path.resolve('src/images/logo.png'),
+                  sizes: [96, 128, 192, 256, 384, 512],
+                  destination: path.join('assets', 'icons'),
+                },
+              ],
+            }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
